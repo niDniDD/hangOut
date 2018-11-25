8826a4f2c9f15338649aedcd7b176e98a6b4c855
niDniDD <oraphin.poo@northbkk.ac.th>
2018-11-25 13:00:35 +0700

    [design]

---
+++ src/app/add-bar/add-bar.page.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-add-bar',
+  templateUrl: './add-bar.page.html',
+  styleUrls: ['./add-bar.page.scss'],
+})
+export class AddBarPage {}
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,3 +1,4 @@
+import { AppPage } from './../../../e2e/src/app.po';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
